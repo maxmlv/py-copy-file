4822366b8c2171bbc4d0371c45f9eb662c80ae9c
maxmlv <max.mlv.k@gmail.com>
2026-06-14 16:51:28 +0300

    fix: improve exception handling of copy_file function

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-import os.path
-
-
 def copy_file(command: str) -> None:
     command_parts = command.split(" ")
-    if len(command_parts) == 3:
-        cp, original, copy = command_parts
-        if cp == "cp" and os.path.exists(original) and original != copy:
-            with open(original, "r") as file_in, open(copy, "w") as file_out:
+    try:
+        if len(command_parts) != 3:
+            raise ValueError("Invalid command: cp <original_file> <new_file>")
+        cmd, source, copy = command_parts
+        if cmd != "cp":
+            raise ValueError("Invalid command: command must be 'cp'")
+        if source != copy:
+            with open(source, "r") as file_in, open(copy, "w") as file_out:
                 file_out.write(file_in.read())
+    except FileNotFoundError as e:
+        print(e)
+    except ValueError as e:
+        print(e)

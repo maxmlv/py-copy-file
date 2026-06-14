2806a44fd98ec677819c3bb0cb4539920a174a21
maxmlv <max.mlv.k@gmail.com>
2026-06-14 16:28:09 +0300

    implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import os.path
+
+
+def copy_file(command: str) -> None:
+    command_parts = command.split(" ")
+    if len(command_parts) == 3:
+        cp, original, copy = command_parts
+        if cp == "cp" and os.path.exists(original) and original != copy:
+            with open(original, "r") as file_in, open(copy, "w") as file_out:
+                file_out.write(file_in.read())
